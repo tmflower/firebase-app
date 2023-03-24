d378e0424f28717c9329bea6916f3e5e05ed46c4
Tina Flower <tmflower@gmail.com>
2023-03-24 12:10:43 -1000

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const userInput = document.getElementById("input-field");
+
+const addButton = document.getElementById("add-button");
+addButton.addEventListener('click', () =>
+	{let inputVal = userInput.value;
+	console.log(inputVal)
+	});
+
